b6da186aa9db32ee77207558863b4ca825502825
humbertomoraes <humbertoccmoraes@yahoo.com.br>
2022-03-18 19:52:09 -0300

    atualizar

--- Aula2/manipulandoDados/scripts/app.js
+++ Aula2/manipulandoDados/scripts/app.js
@@ -1,6 +1,3 @@
-let um = "Pedra"
-let dois = "Papel"
-
 let jogador1 = parseInt(prompt(" Digite 1 para Pedra\n Digite 2 para Papel\n Digite 3 para Tesoura"))
 document.querySelector('.jog1').innerText += ' ' + jogador1
 
@@ -9,17 +6,17 @@ let computador = parseInt(Math.random() * 3 + 1)
 document.querySelector('.jog2').innerText += ' ' + computador
 
 if (jogador1 == 1 && computador == 2) {
-    alert("Ganhou jogador 2 - Papel embrulha a Pedra");
+    document.querySelector('.Resultado').innerText += ' ' + "Ganhou o computador - Papel embrulha a Pedra"
 } else if (jogador1 == 1 && computador == 3) {
-    alert("Ganhou jogador 1 - Pedra quebra a Tesoura");
+    document.querySelector('.Resultado').innerText += ' ' + "Ganhou o jogador 1 - Pedra quebra a Tesoura"
 } else if (jogador1 == 2 && computador == 1) {
-    alert("Ganhou jogador 1 - Papel embrulha a Pedra");
+    document.querySelector('.Resultado').innerText += ' ' + "Ganhou o jogador 1 - Papel embrulha a Pedra"
 } else if (jogador1 == 2 && computador == 3) {
-    alert("Ganhou jogador 2 - Tesoura corta o Papel");
+    document.querySelector('.Resultado').innerText += ' ' + "Ganhou o computador - Tesoura corta o Papel"
 } else if (jogador1 == 3 && computador == 1) {
-    alert("Ganhou jogador 2 - Pedra quebra a Tesoura");
+    document.querySelector('.Resultado').innerText += ' ' + "Ganhou o computador - Pedra quebra a Tesoura"
 } else if (jogador1 == 3 && computador == 2) {
-    alert("Ganhou jogador 1 - Tesoura corta o Papel");
+    document.querySelector('.Resultado').innerText += ' ' + "Ganhou o jogador 1 - Tesoura corta o Papel"
 } else {
-    alert("Empatou");
+    document.querySelector('.Resultado').innerText += ' ' + "Empatou<<<<"
 }
